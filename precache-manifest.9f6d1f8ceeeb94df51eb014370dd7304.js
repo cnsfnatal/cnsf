4f06717e618cdaee4416d2de991e9c2b74041b4a
Pedro Henrique <pedrotrab2132@gmail.com>
2020-02-27 11:32:16 -0300

    Updates

---
+++ precache-manifest.9f6d1f8ceeeb94df51eb014370dd7304.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3ebaf5cb785824f62334fcc27b528d",
+    "url": "/cnsf/index.html"
+  },
+  {
+    "revision": "70f51a22fd132f1118f3",
+    "url": "/cnsf/static/css/main.91fcf66a.chunk.css"
+  },
+  {
+    "revision": "850d8799012092afb2b4",
+    "url": "/cnsf/static/js/2.5506ec19.chunk.js"
+  },
+  {
+    "revision": "70f51a22fd132f1118f3",
+    "url": "/cnsf/static/js/main.4f93d413.chunk.js"
+  },
+  {
+    "revision": "055d35a65913ec64e264",
+    "url": "/cnsf/static/js/runtime-main.db14919a.js"
+  },
+  {
+    "revision": "ae9e21f5cf9ea2c2643eb8c6dfd0bac6",
+    "url": "/cnsf/static/media/1.ae9e21f5.jpg"
+  },
+  {
+    "revision": "23f4f6c116d3fdf9980acd98b184602a",
+    "url": "/cnsf/static/media/2.23f4f6c1.jpg"
+  },
+  {
+    "revision": "9e90b401da24abce7f60d292f0d67e93",
+    "url": "/cnsf/static/media/3.9e90b401.jpg"
+  },
+  {
+    "revision": "a7eb41923d120c9973742501fadde997",
+    "url": "/cnsf/static/media/background.a7eb4192.png"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/cnsf/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "40dc8dce4dae2824e9ace2798cfeada3",
+    "url": "/cnsf/static/media/logo-cnsf.40dc8dce.png"
+  },
+  {
+    "revision": "776ca80c9e4cc217af6f74fee962e08e",
+    "url": "/cnsf/static/media/metologia-image.776ca80c.png"
+  },
+  {
+    "revision": "a7eb41923d120c9973742501fadde997",
+    "url": "/cnsf/static/media/scholae_background2.a7eb4192.png"
+  },
+  {
+    "revision": "d83f15b80c968a5a0f408d1e12681bf9",
+    "url": "/cnsf/static/media/school.d83f15b8.svg"
+  },
+  {
+    "revision": "855adeac81c7ea5413aa0f894c76a301",
+    "url": "/cnsf/static/media/study.855adeac.svg"
+  },
+  {
+    "revision": "fd0dee780c3c245bc722f6b1499e3dcd",
+    "url": "/cnsf/static/media/teacher.fd0dee78.svg"
+  },
+  {
+    "revision": "e44c8f30f0ba031fbcc48e09bb9baf24",
+    "url": "/cnsf/static/media/training.e44c8f30.svg"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/cnsf/static/media/twitter.0251d8ee.png"
+  },
+  {
+    "revision": "3773db94ccd0e5b78b5400410a12f86f",
+    "url": "/cnsf/static/media/video_slide.3773db94.mp4"
+  }
+]);
