3a29558833f7397d09ae9c57a223410d5615b17f
Pedro Henrique <pedrotrab2132@gmail.com>
2020-02-27 19:29:59 -0300

    api config

--- src/pages/início/contato/index.js
+++ src/pages/início/contato/index.js
@@ -1,72 +1,85 @@
-import React, {  } from 'react';
-
-
+import React, { useState } from "react";
+import api from "../../../services/api";
 
 // Styles
-import './contato.scss';
-
+import "./contato.scss";
 
 // Components
 
 function Contato_Home(data) {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+  const [celular, setCelular] = useState("");
 
-        async function handleSubmit(e) {
-            e.preventDefault()
-        }
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    
-        return (
-            <div className="contato-home-container">
+    const sendEmail = await api.post("/send-email", {
+      email: email,
+      nome: nome,
+      mensagem: `Mensagem: ${mensagem} 
+                   Email: ${email}
+                   Celular: ${celular}`
+    });
 
+    alert(`Recebemos sua mensagem ${nome}, entraremos em contato em breve.`);
+    console.log(sendEmail);
+  }
 
-                <div className="form-contato">
-                    <div className="form-title-container-home">
-                        <h1 className="title-form">Faça sua <br></br> pré-matrícula</h1>
-                    </div>
-                    
-                    <form className="form-container" onSubmit={handleSubmit}>
+  return (
+    <div className="contato-home-container">
+      <div className="form-contato">
+        <div className="form-title-container-home">
+          <h1 className="title-form">
+            Faça sua <br></br> pré-matrícula
+          </h1>
+        </div>
 
-                        <input
-                            className="input-form-home"
-                            type="text"
-                            name="email"
-                            placeholder="Seu email"
-                            required
-                        />
+        <form className="form-container" onSubmit={handleSubmit}>
+          <input
+            className="input-form-home"
+            type="text"
+            name="email"
+            placeholder="Seu email"
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
 
-                        <input
-                            className="input-form-home"
-                            type="text"
-                            name="nome"
-                            placeholder="Seu nome"
-                            required
-                        />
+          <input
+            className="input-form-home"
+            type="text"
+            name="nome"
+            placeholder="Seu nome"
+            onChange={e => setNome(e.target.value)}
+            required
+          />
 
-                        <input
-                            className="input-form-home"
-                            type="text"
-                            name="escola"
-                            placeholder="Nome da escola"
-                            required
-                        />
-                        
-                        <input
-                            className="msg-form-home"
-                            type="text"
-                            name="mensagem"
-                            placeholder="Mensagem"
-                            required
-                        />
-                        
+          <input
+            className="input-form-home"
+            type="text"
+            name="celular"
+            placeholder="Seu celular"
+            onChange={e => setCelular(e.target.value)}
+            required
+          />
 
-                        <button type="submit" className="btn-enviar-form-home">Enviar</button>
-                    </form>
-                </div>
+          <input
+            className="msg-form-home"
+            type="text"
+            name="mensagem"
+            placeholder="Mensagem"
+            onChange={e => setMensagem(e.target.value)}
+            required
+          />
 
-
-
-            </div>
-    )
+          <button type="submit" className="btn-enviar-form-home">
+            Enviar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contato_Home
+export default Contato_Home;
