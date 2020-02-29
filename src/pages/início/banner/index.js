30af1c90244eeef7ca162dddf65126de813f258a
Pedro Henrique <pedrotrab2132@gmail.com>
2020-02-29 15:40:17 -0300

    banner video 2

--- src/pages/início/banner/index.js
+++ src/pages/início/banner/index.js
@@ -1,43 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Styles
-import './banner.scss';
+import "./banner.scss";
 
 // Media
-import Logo from '../../../img/logo-cnsf.png';
-import Video from "./video_slide.mp4";
-
+import Logo from "../../../img/logo-cnsf.png";
+//import Video from "./video_slide.mp4";
+import Video_1 from "./cnsf_1.mp4";
 
 export default class Banner extends Component {
-    render() {
-        return (
-            <div className="banner-container">
-                
+  render() {
+    return (
+      <div className="banner-container">
+        <video autoPlay muted loop className="video-banner">
+          <source src={Video_1} type="video/mp4" />
+        </video>
 
-                    
+        <div className="text-banner-container">
+          <p className="title-banner">Colégio Nossa Senhora de Fátima</p>
+          <p className="text-banner">Uma educação para a inovação!</p>
+        </div>
 
-                    <video  autoPlay muted loop className="video-banner">
-                        <source src={Video} type="video/mp4" />
-                        
-                    </video>
-
-                    <div className="text-banner-container">
-                        <p className="title-banner">Colégio Nossa Senhora de Fátima</p>
-                        <p className="text-banner">Uma educação para a inovação!</p>
-                    </div>
-
-                    <div className="banner-imageee">
-                        <img src={Logo} className="image-banneeer" alt="cnsf"/>
-                    </div>
-                    
-
-                    
-
-                
-                
-
-                
-            </div>
-        )
-    }
+        <div className="banner-imageee">
+          <img src={Logo} className="image-banneeer" alt="cnsf" />
+        </div>
+      </div>
+    );
+  }
 }
