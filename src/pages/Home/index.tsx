87467a72a9f296e4c32ea91611c117e178f20af6
pedroksty <pedrotrab2132@gmail.com>
2020-09-17 23:38:08 -0300

    Create About and Info components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,8 +6,21 @@ import Layout from '../../components/Layout'
 
 import Video from '../../assets/cnsf_video.mp4'
 import logo from '../../assets/logo.png'
+import metologiaImage from '../../assets/metologia-image.png'
+import smile from '../../assets/smile.png'
+import idea from '../../assets/idea.png'
+import lamp from '../../assets/lamp.png'
 
-import { Container, Header, VideoArea } from './styles'
+import {
+  Container,
+  Header,
+  VideoArea,
+  AboutContainer,
+  ImageContent,
+  TextContainer,
+  InfoContainer,
+  InfoCard
+} from './styles'
 
 const Home: React.FC = () => {
   return (
@@ -28,6 +41,49 @@ const Home: React.FC = () => {
           <source src={Video} type="video/mp4" />
         </video>
       </VideoArea>
+
+      <AboutContainer>
+        <ImageContent>
+          <img src={metologiaImage} alt="Crianças se divertindo" />
+        </ImageContent>
+
+        <TextContainer>
+          <strong>Metologia</strong>
+          <p>
+            Acreditamos que a escola deve ser o lugar onde os alunos despertem
+            sua criatividade, deem vidas aos seus sonhos e construam o seu
+            próprio futuro. Além disso, atuamos com abordagens de ensino capazes
+            de estimular diversificados pensamento na construção de
+            conhecimentos, projetos e propósitos de vida!
+          </p>
+        </TextContainer>
+      </AboutContainer>
+
+      <InfoContainer>
+        <InfoCard>
+          <strong>Competências emocionais</strong>
+          <img src={smile} alt="Sorriso" />
+          <p>
+            o trabalho em equipe, a empatia, a comunicação são pilares
+            fundamentais para o crescimento pessoal e profissional
+          </p>
+        </InfoCard>
+
+        <InfoCard>
+          <strong>Mudança de Pensamento</strong>
+          <img src={idea} alt="Ideia" />
+          <p>despertando criações sem barreiras</p>
+        </InfoCard>
+
+        <InfoCard>
+          <strong>Ambientes de inovação</strong>
+          <img src={lamp} alt="Lâmpada" />
+          <p>
+            oficinas em ambientes reais, possibilitando novas crenças e quebra
+            de paradigmas.
+          </p>
+        </InfoCard>
+      </InfoContainer>
     </Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import background from '../../assets/background.png'
 
 export const Container = styled.div`
   width: 100%;
@@ -21,4 +22,105 @@ export const VideoArea = styled.div`
   width: 100%;
   overflow: hidden;
   z-index: 1;
+`
+export const AboutContainer = styled.div`
+  width: 100%;
+  padding: 100px 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  background: #fff;
+  background-image: url(${background});
+
+  @media (max-width: 990px) {
+    padding: 20px 10px;
+    flex-direction: column;
+  }
+`
+
+export const ImageContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+
+  img {
+    width: 440px;
+    height: 650px;
+
+    @media (max-width: 990px) {
+      width: 230px;
+      height: 340px;
+    }
+  }
+`
+export const TextContainer = styled.div`
+  background: #fff;
+  width: 30%;
+  border-radius: 15px;
+
+  padding: 20px;
+
+  @media (max-width: 990px) {
+    width: 95%;
+  }
+
+  > strong {
+    font-size: 52px;
+    color: #c72d22;
+  }
+
+  > p {
+    font-size: 26px;
+    color: #575050;
+  }
+`
+
+export const InfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 50px;
+  background: #6861f2;
+
+  @media (max-width: 990px) {
+    flex-direction: column;
+  }
+`
+
+export const InfoCard = styled.div`
+  margin: 20px auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 15px;
+  max-width: 400px;
+  height: 480px;
+
+  @media (max-width: 990px) {
+    height: auto;
+  }
+
+  > strong {
+    font-size: 36px;
+    color: #efeeef;
+    text-align: center;
+  }
+
+  > img {
+    height: 150px;
+    width: 150px;
+    margin: 22px 0;
+  }
+
+  > p {
+    text-align: center;
+    color: #efeeef;
+    font-size: 28px;
+  }
 `
